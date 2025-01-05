2c13b2ba2f10729b87741e970032267cad8a91da
Navatusein <boghdan.kutsulima@gmail.com>
2025-01-05 13:09:54 +0200

    Add not found page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,10 @@
 import {FC} from "react";
-import {Route, Routes} from "react-router";
+import {Navigate, Route, Routes} from "react-router";
 import {HomePage} from "@/pages/home";
 import {ConfiguratorPage} from "@/pages/configurator";
 import BaseLayout from "@/app/layouts/base-layout/base-layout.tsx";
+import {NotFoundPage} from "@/pages/not-found";
+
 
 const Router: FC = () => {
   return (
@@ -10,7 +12,8 @@ const Router: FC = () => {
       <Route element={<BaseLayout/>}>
         <Route index element={<HomePage/>}/>
         <Route path="/configurator" element={<ConfiguratorPage/>}/>
-        <Route path='*' element={404}/>
+        <Route path="/404" element={<NotFoundPage/>}/>
+        <Route path='*' element={<Navigate to="/404" replace/>}/>
       </Route>
     </Routes>
   );
