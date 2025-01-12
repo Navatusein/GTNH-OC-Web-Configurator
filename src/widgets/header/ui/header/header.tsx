ca16344decb0fd4e33af2bae4ebc23b039dfa9e5
Navatusein <boghdan.kutsulima@gmail.com>
2025-01-12 17:20:17 +0200

    Added dev page

--- src/widgets/header/ui/header/header.tsx
+++ src/widgets/header/ui/header/header.tsx
@@ -2,6 +2,8 @@ import {FC} from "react";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {useNavigate} from "react-router";
 
+const IS_DEV = true;
+
 const Header: FC = () => {
   const navigate = useNavigate();
 
@@ -16,6 +18,11 @@ const Header: FC = () => {
               <Nav.Link onClick={() => navigate("/")}>
                 Home
               </Nav.Link>
+              {IS_DEV &&
+                <Nav.Link onClick={() => navigate("/dev")}>
+                  Dev
+                </Nav.Link>
+              }
               <Nav.Link onClick={() => navigate("/create-link")}>
                 Create link
               </Nav.Link>
